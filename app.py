685ed16bb5abd6f2d253ea2ae203e3f96e764d26
jonathanwickcodes <johnwickerel@gmail.com>
2025-11-11 23:59:32 +0530

    11 - 12 - btn

--- app.py
+++ app.py
@@ -649,7 +649,7 @@ def main_page():
                                    height=100)
 
         st.markdown('<div class="apple-primary-button-container" style="display: flex; justify-content: center;">', unsafe_allow_html=True)
-        submitted = st.form_submit_button("Let's Start!", type="info", disabled=not GEMINI_ENABLED)
+        submitted = st.form_submit_button("Let's Start!", type="primary", disabled=not GEMINI_ENABLED)
         st.markdown('</div>', unsafe_allow_html=True)
 
         if submitted:
